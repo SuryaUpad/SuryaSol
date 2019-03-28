26f5b7b0f2a45610ab6eeba3d4bfe2eb6e503750
SuryaUpad <49033332+SuryaUpad@users.noreply.github.com>
2019-03-28 16:04:00 +0000

    Surya Solution

--- FileData/Program.cs
+++ FileData/Program.cs
@@ -9,6 +9,45 @@ namespace FileData
     {
         public static void Main(string[] args)
         {
+
+
+
+            if (args != null && args.Length != 2)
+            {
+                Console.WriteLine("sInvalid arguments are passed");
+                return;
+            }
+            if (args[0] != "v" && args[0] != "s")
+            {
+                Console.WriteLine("Invalid arguments are passed");
+                return;
+            }
+            switch (args[0])
+            {
+                case "v":
+                    Console.WriteLine(getFileVersion(args[1]));
+                    break;
+                case "s":
+                    Console.WriteLine(getFileSize(args[1]));
+                    break;
+                default:
+                    break;
+            }
+            Console.ReadLine();
+        }
+
+
+        private static string getFileSize(string filePath)
+        {
+            var fd = new FileDetails();
+
+            return fd.Size(filePath).ToString();
+        }
+        private static string getFileVersion(string filePath)
+        {
+            var fd = new FileDetails();
+
+            return fd.Version(filePath);
         }
     }
 }
